c319baa9aea6968b043f49bdf0ba96a83e8669d9
Robson Heinke <robsonheinke@hotmail.com>
2017-06-13 23:08:07 -0300

    teste

---
+++ MavenRobson/src/main/java/model/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+
+/**
+ *
+ * @author marce
+ */
+public class Main {
+    public static void main(String[] args){
+        Criptografia criptografia = new Criptografia();
+        System.out.println(criptografia.criptografar("estou testando uma frase"));
+    }
+}
