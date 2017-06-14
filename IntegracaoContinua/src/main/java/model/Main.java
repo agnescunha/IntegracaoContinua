61d3af2f48aa09ec944db913f4efe293618c28ae
Robson Heinke <robsonheinke@hotmail.com>
2017-06-14 01:16:07 -0300

    6

--- IntegracaoContinua/src/main/java/model/Main.java
+++ IntegracaoContinua/src/main/java/model/Main.java
@@ -14,6 +14,6 @@ public class Main {
     public static void main(String[] args){
         Conversor conversor = new Conversor();
         System.out.println(conversor.converterBinario("estou testando uma frase"));
-        System.out.println("teste");
+        System.out.println(conversor.converterBinario("testando"));
     }
 }
